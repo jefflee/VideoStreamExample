8a74cc1a48cea4ae3264cb846e7b78173dd21db1
Jeff Lee <hellow.jeff@gmail.com>
2020-09-09 10:04:25 +0800

    Video stream API

---
+++ VideoStreamExample/Controllers/VideoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VideoStreamExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        [HttpGet("movie", Name = "Get a movie")]
+        public FileResult GetTestMovie()
+        {
+            return PhysicalFile($"D:\\documents\\temp\\test2.mp4", "application/octet-stream",
+                enableRangeProcessing: true);
+        }
+    }
+}
